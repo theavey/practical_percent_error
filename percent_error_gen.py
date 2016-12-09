2f79f628c83c08848f706ba142a694d0f25e4c65
Thomas Heavey <thomasjheavey@gmail.com>
2016-12-09 13:24:22 -0500

    Shortened code, made it look nicer, calls python better

--- percent_error_gen.py
+++ percent_error_gen.py
@@ -1,43 +1,36 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 
-def calc_and_display_l1(correct_value):
-    try:
-        while True:
-            their_value = float(input('Their value: '))
-            percent_error = abs(their_value - correct_value) / correct_value
-            grade = 26 - round(percent_error * 100.0 / 2)
-            if grade < 0:
-                grade = 0
-            print('percent error: {:.2%}'.format(percent_error))
-            print('grade: {}'.format(grade))
-    except ValueError:
-        return None
-
-
-def calc_and_display_l2(correct_value):
+def calc_and_display(correct_value, section):
     try:
         their_value = float(input('Their value: '))
         percent_error = abs(their_value - correct_value) / correct_value
         grade = 26 - round(percent_error * 100.0 / 2)
         if grade < 0:
             grade = 0
-        print('percent error: {:.2%}'.format(percent_error))
-        print('grade: {}'.format(grade))
+        # Molarity x volume x (mg / mol)
+        true_10mL = correct_value * 0.010 * 63546.0
+        their_10mL = their_value * 0.010 * 63546.0
+        print('\npercent error: {:.2%}'.format(percent_error))
+        print('\ngrade: {}\n'.format(grade))
+        print('Their value in 10 mL: {:.2f} mg.'.format(their_10mL))
+        print('True value in 10 mL: {:.2f} mg.\n'.format(true_10mL))
+        if section == 1:
+            calc_and_display(correct_value, 1)
     except ValueError:
-        print('Invalid input. Retry entering a number.')
-        calc_and_display_l2()
+        return None
 
 
 def control():
     while True:
-        key = input("'0' for L1, '1' for L2 \#1, '2' for L2 \#2, 'exit' to exit: ")
+        key = input(" '0' for L1\n '1' for L2 #1\n "
+                    "'2' for L2 #2\n 'exit' to exit\n Choice: ")
         if key == '0':
-            calc_and_display_l1(0.0181659)
+            calc_and_display(0.0181659, 1)
         elif key == '1':
-            calc_and_display_l2()
+            calc_and_display(0.02264, 2)
         elif key == '2':
-            calc_and_display_l2()
+            calc_and_display(0.01767, 2)
         elif key == 'exit':
             break
         else:
