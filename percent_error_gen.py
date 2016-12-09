6418faf2e6f858fb1ebe1d9c54f72380719fa07d
Thomas Heavey <thomasjheavey@gmail.com>
2016-12-09 16:30:28 -0500

    Rearranged for smaller try-except clause

--- percent_error_gen.py
+++ percent_error_gen.py
@@ -26,22 +26,24 @@
 def calc_and_display(correct_value, section):
     try:
         their_value = float(input('Their value: '))
-        percent_error = abs(their_value - correct_value) / correct_value
-        grade = 26 - round(percent_error * 100.0 / 2)
-        if grade < 0:
-            grade = 0
-        # Molarity x volume x (mg / mol)
-        true_10mL = correct_value * 0.010 * 63546.0
-        their_10mL = their_value * 0.010 * 63546.0
-        print('\n\nTrue value: {:.4f}'.format(correct_value))
-        print('\npercent error: {:.2%}'.format(percent_error))
-        print('\ngrade: {}\n'.format(grade))
-        print('Their value in 10 mL: {:.2f} mg.'.format(their_10mL))
-        print('True value in 10 mL: {:.2f} mg.\n'.format(true_10mL))
-        if section == 1:
-            calc_and_display(correct_value, 1)
     except ValueError:
         return None
+    percent_error = abs(their_value - correct_value) / correct_value
+    grade = 26 - round(percent_error * 100.0 / 2)
+    if grade < 0:
+        grade = 0
+    # Molarity x volume x (mg / mol)
+    true_10mL = correct_value * 0.010 * 63546.0
+    their_10mL = their_value * 0.010 * 63546.0
+    print('\n\nTrue value: {:.4f}'.format(correct_value))
+    print('\npercent error: {:.2%}'.format(percent_error))
+    print('\ngrade: {}\n'.format(grade))
+    print('Their value in 10 mL: {:.2f} mg.'.format(their_10mL))
+    print('True value in 10 mL: {:.2f} mg.\n'.format(true_10mL))
+    if section == 1:
+        calc_and_display(correct_value, 1)
+    return None
+
 
 
 def control():
